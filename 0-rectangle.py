3acd8caee8ac7e847eea3d55eed52af5db024eb4
Peter Chibunna <peterchibunna@gmail.com>
2022-10-17 14:09:22 +0100

    init

---
+++ 0-rectangle.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""
+The Rectangle module
+"""
+
+class Rectangle(object):
+	""" A simple Rectangle class """
+	pass
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# 0x08-python-more_classes
+# 0x07-python-more_classes
